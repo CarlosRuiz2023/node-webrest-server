5880f46c3c13fc464bd01044f0aa7c4c317a2608
CarlosRuiz2023 <juancarlosruizgomez2000@gmail.com>
2024-03-01 13:48:29 -0600

    first commit

---
+++ dist/app.js
@@ -0,0 +1,32 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = __importDefault(require("http"));
+const fs_1 = __importDefault(require("fs"));
+const server = http_1.default.createServer((req, res) => {
+    var _a;
+    console.log(req.url);
+    //   res.writeHead(200, { "Content-Type": "text/html" });
+    //   res.write(`<h1>URL ${req.url}</h1>`);
+    //   res.end();
+    //   const data = { name: "John Doe", age: 30, city: "New York" };
+    //   res.writeHead(200, { "Content-Type": "application/json" });
+    //   res.end(JSON.stringify(data));
+    if (req.url === "/") {
+        const htmlFile = fs_1.default.readFileSync("./public/index.html", "utf-8");
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(htmlFile);
+        return;
+    }
+    if ((_a = req.url) === null || _a === void 0 ? void 0 : _a.endsWith(".js")) {
+        res.writeHead(200, { "Content-Type": "application/javascript" });
+    }
+    else {
+        res.writeHead(200, { "Content-Type": "text/css" });
+    }
+});
+server.listen(8080, () => {
+    console.log("Server running on port 8080");
+});
